ed359784ef9e45d98c9b714168da4bc74f71f11c
mitesh <mitesh.vishwakarma@gmail.com>
2018-03-29 17:07:51 +0530

    candidate page updated

--- candidate.php
+++ candidate.php
@@ -3,4 +3,40 @@
 include './local_resources/connections/database.php';
 // including the header
 require_once './local_resources/components/header.php';
+
+//starting session
+session_start();
+
+//get the candidate name
+$canName=$_GET["candidateName"];
+
+// connect database
+databaseConnection();
+
+// get the selected candidate details
+$candidateArray=candidateConnection($canName);
+
+
+
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php require_once './local_resources/components/footer.php';?>
